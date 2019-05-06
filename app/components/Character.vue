9d8a011c420bc8192277c483e2906a4f964078cb
LimongiVirgil <limongi.virgil@gmail.com>
2019-05-06 11:28:55 +0200

    add json

--- app/components/Character.vue
+++ app/components/Character.vue
@@ -2,7 +2,7 @@
   <div class="big-header">
     <h1>Character</h1>
     <br />
-    <router-link class="button" to="/game/:id">Go to Game</router-link>
+    <router-link class="button" to="/game/1">Go to Game</router-link>
   </div>
 </template>
 
--- app/components/Game.vue
+++ app/components/Game.vue
@@ -2,17 +2,28 @@
   <div class="big-header">
     <h1>Game</h1>
     <br />
-    <router-link class="button" to="/Win">Go to Win</router-link>
-    <router-link class="button" to="/Lose">Go to Lose</router-link>
+    <router-link class="button" :to="actionA">{{ message }}</router-link>
+    <router-link class="button" :to="actionB" v-if="actionB">{{ message }}</router-link>
   </div>
 </template>
 
 <script>
+import game from '../data/data.json';
+
 export default {
-  data() {
-    return {
-      message: 'Hello Hetic'
-    };
-  }
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+    message() {
+      return game[this.id].message;
+    },
+    actionA() {
+      return game[this.id].actionA;
+    },
+    actionB() {
+      return game[this.id].actionB;
+    },
+  },
 };
 </script>
