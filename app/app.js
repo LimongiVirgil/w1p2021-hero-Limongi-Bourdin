b9f9b808c3ed45508b95791607894aff2a394d75
LimongiVirgil <limongi.virgil@gmail.com>
2019-05-21 08:52:09 +0200

    add music

--- app/app.js
+++ app/app.js
@@ -3,3 +3,7 @@ import Vue from 'vue';
 import router from './router';
 
 new Vue({ router: router }).$mount('#root');
+
+let sound = document.getElementById('sound');
+sound.play();
+sound.volume = 1;
